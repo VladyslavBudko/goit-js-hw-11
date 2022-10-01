5d3ae24e7619ca49a1d07eb10dc6f0b8f33dd9c9
Vladyslav Budko <vladyslav.budko@gmail.com>
2022-10-01 23:44:00 +0300

    add Notiflix, url

--- src/index.js
+++ src/index.js
@@ -1,42 +1,25 @@
 import axios from 'axios';
-// const axios = require('axios');
-
-// // Делаем запрос пользователя с данным ID
-// axios.get('/user?ID=12345')
-//   .then(function (response) {
-//     // обработка успешного запроса
-//     console.log(response);
-//   })
-//   .catch(function (error) {
-//     // обработка ошибки
-//     console.log(error);
-//   })
-//   .then(function () {
-//     // выполняется всегда
-//   });
-
-// По желанию вышеуказанный запрос можно выполнить так
-axios
-  .get('/user', {
-    params: {
-      ID: 12345,
-    },
-  })
-  .then(function (response) {
-    console.log('response 1:', response);
-  })
-  .catch(function (error) {
-    console.log(error);
-  })
-  .then(function () {
-    // выполняется всегда
-  });
+import Notiflix from 'notiflix';
 
 
-  
+const MAIN_URL = 'https://pixabay.com/api/';
+const API_KEY = '30279426-ce0edf6a31bb607e668c5bb01';
+let query = '';
+const OPT_URL = `q=${query}&image_type=photo&orientation=horizontal&safesearch=true`;
+
+let URL = MAIN_URL+API_KEY+OPT_URL;
+
+// $.getJSON(URL, function(data){
+// if (parseInt(data.totalHits) > 0)
+//     $.each(data.hits, function(i, hit){ console.log(hit.pageURL); });
+// else
+//     console.log('No hits');
+// });
+
+
 async function getUser() {
   try {
-    const response = await axios.get('/user?ID=12');
+    const response = await axios.get(URL);
     console.log('response in assync:', response);
     // const userID = awaid response.json();
     return response;
@@ -46,7 +29,7 @@ async function getUser() {
 }
 
 getUser()
-  .then( response => {
+  .then(response => {
     console.log('response in getUser:', response);
     return response;
   })
@@ -58,15 +41,9 @@ getUser()
     // выполняется всегда
   });
 
-
-  axios.get('/user', {
-    firstName: 'Fred',
-    lastName: 'Flintstone'
-  })
-  .then(function (response) {
-    console.log(response);
-  })
-  .catch(function (error) {
-    console.log(error);
-  });
-
+function onFetchError(error) {
+  resetInnerHTML();
+  Notiflix.Notify.failure(
+    'Sorry, there are no images matching your search query. Please try again.'
+  );
+}
